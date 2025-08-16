9f9ea5a8f5c6996d50d04ef9d6c8d564ba4533ef
?0 <benjamindguntara@protonmail.com>
2025-08-16 08:32:01 +0000

    jdsaoidjasiojd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,21 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({  
     plugins: [    
         tailwindcss(),  
-       ],
+    ],
+    build: {
+        outDir: '../static/dist',
+        emptyOutDir: true,
+
+        rollupOptions: {
+            input: {
+                main: './src/main.js',
+                style: './src/style.css'
+            },
+            output: {
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name][extname]'
+            }
+        },
+    }
 })
